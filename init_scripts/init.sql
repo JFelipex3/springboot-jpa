73629670405761f288a0f9fac5a5cbb45f854bd3
JFelipex3 <jfelipe.machuca@gmail.com>
2025-07-24 23:54:35 -0400

    Se crea docker-compose, script init.sql y ajuste en application.properties

---
+++ init_scripts/init.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS persons (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    lastname VARCHAR(255),
+    programming_language VARCHAR(255)
+);
+
+INSERT INTO persons (name, lastname, programming_language) VALUES ('Andres', 'Guzman', 'Java');
+INSERT INTO persons (name, lastname, programming_language) VALUES ('Pepe', 'Doe', 'Python');
+INSERT INTO persons (name, lastname, programming_language) VALUES ('John', 'Dow', 'JavaScript');
+INSERT INTO persons (name, lastname, programming_language) VALUES ('Maria', 'Roe', 'Java');
+INSERT INTO persons (name, lastname, programming_language) VALUES ('Josefa', 'Rae', 'Java');
