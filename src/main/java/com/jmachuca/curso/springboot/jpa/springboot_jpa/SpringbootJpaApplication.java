29c1a9829accdcbd5417294a79c1573adc43fb10
JFelipex3 <jfelipe.machuca@gmail.com>
2025-07-25 00:24:45 -0400

    Reapply "Crear clase Repository y ajustar clase run"
    This reverts commit 0bb2c777180d51f8ff0f2eb0beab78257d17ca26.

--- src/main/java/com/jmachuca/curso/springboot/jpa/springboot_jpa/SpringbootJpaApplication.java
+++ src/main/java/com/jmachuca/curso/springboot/jpa/springboot_jpa/SpringbootJpaApplication.java
@@ -1,19 +1,30 @@
 package com.jmachuca.curso.springboot.jpa.springboot_jpa;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.jmachuca.curso.springboot.jpa.springboot_jpa.entities.Person;
+import com.jmachuca.curso.springboot.jpa.springboot_jpa.repositories.PersonRepository;
+
 @SpringBootApplication
 public class SpringbootJpaApplication implements CommandLineRunner{
 
+	@Autowired
+	private PersonRepository personRepository; 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootJpaApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		throw new UnsupportedOperationException("Unimplemented method 'run'");
+		
+		List<Person> persons = (List<Person>) personRepository.findAll();
+
+		persons.stream().forEach(person -> System.out.println(person));
 	}
 
 }
